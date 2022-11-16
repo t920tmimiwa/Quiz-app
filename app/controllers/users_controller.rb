b2bd0e5a38d7e15100a8363d5ef8b5ead817dc92
Ubuntu <ubuntu@ip-172-31-79-19.ec2.internal>
2022-11-16 08:26:26 +0000

    ユーザ登録機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    user = User.new(uname: params[:user][:uname], pass: params[:user][:pass])
+    if user.save
+      redirect_to root_path
+    else
+      render new_user_path
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+  
 end
