f0b708b20e5452b1cc757d644a28e8c28fe40a85
2011140023 <ubuntu@ip-172-31-46-180.ec2.internal>
2022-12-13 17:47:07 +0000

    答え4択

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,8 +9,12 @@ class QuestionsController < ApplicationController
     
     def create
         que = params[:question][:que]
+        answer1 = params[:question][:answer1]
+        answer2 = params[:question][:answer2]
+        answer3 = params[:question][:answer3]
+        answer4 = params[:question][:answer4]
         #user = User.find_by(uid: session[:login_uid])
-        @question = Question.new(que: que)#, user_id: user.id)
+        @question = Question.new(que: que, answer1: answer1, answer2: answer2, answer3: answer3, answer4: answer4)#, user_id: user.id)
         if @question.save
             redirect_to root_path
         else
